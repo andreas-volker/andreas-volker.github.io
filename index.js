020597cd2836dd0b57284725239fd0fee6fa04ab
Andreas K. Völker <andreas.volker@shiftinc.com.br>
2018-06-05 12:32:00 -0300

    js console

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 (function(window, document, undefined) {
     'use strict';
-    var NEUA = window.NEUA = window.NEUA || {};
+    var NEUA = (window.NEUA = (window.NEUA || {}));
     NEUA.on = {};
     NEUA.on.ready = function() {
         var initing = true,
@@ -11,11 +11,13 @@
                 return;
             initing = false;
             NEUA.main(window, document, NEUA, undefined);
+            console.log('initing');
         }
 
         function load() {
             if (document.readyState !== 'complete' && !loaded)
                 return;
+            console.log('loaded');
             loaded = true;
             if (window.location.hash) {
                 window.setTimeout(function() {
@@ -28,32 +30,22 @@
                 init();
             }
         }
-        // window.addEventListener('beforeunload', NEUA.on.beforeunload, false);
+        window.onbeforeunload = NEUA.on.beforeunload;
         window.addEventListener('load', load, false);
         load();
     };
     NEUA.on.beforeunload = function() {
+        console.log('beforeunload');
         window.scrollTo(0, 0);
         document.body.scrollTop = 0;
-        return undefined;
+        return null;
     };
     NEUA.on.resize = function() {
-        var body = document.body,
-            h = window.innerHeight + 'px';
-        if (body) {
-            if (!body.style.height) {
-                h = window.screen.height + 'px';
-                window.setTimeout(function() {
-                    window.scrollTo(0, window.screen.height);
-                }, 0);
-            }
-            body.style.height = h;
-        }
     };
     NEUA.main = function main(window, document, NEUA, undefined) {
-        // window.addEventListener('resize', NEUA.on.resize, false);
-        // window.addEventListener('orientationchange', NEUA.on.resize, false);
-        // NEUA.on.resize();
+        window.addEventListener('resize', NEUA.on.resize, false);
+        window.addEventListener('orientationchange', NEUA.on.resize, false);
+        NEUA.on.resize();
     };
     if (!NEUA)
         return;
